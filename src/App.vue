2b0401307ab19462ee77fa0dde266e13a7d26bf5
Manish <sth_manis@hotmail.com>
2025-04-03 19:12:08 +0200

    1st part done

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script>
+import Header from "@/components/Header.vue";
+export default {
+  components: { Header },
+  name: "App",
+};
+</script>
+
+<template>
+  <Header />
+  <div id="app">
+    <img src="@/images/vue-logo.png" alt="vuelogo" class="vue-logo" />
+    <h1>Welcome to Vue.js</h1>
+    <h3 class="h3-intro">
+      Become a coding ninja with the fatest-growing JavaScript framework
+    </h3>
+    <p class="btn-text-intro">Awesome!</p>
+  </div>
+</template>
+
+
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 25px;
+}
+.vue-logo {
+  width: 129px;
+  height: 129px;
+}
+h1 {
+  font-weight: 800;
+  font-size: 50px;
+  color: #2b2f30;
+}
+h3 {
+  font-weight: 800;
+  font-size: 14px;
+  color: #000000;
+}
+.h3-intro {
+  width: 309px;
+  height: 40px;
+  text-align: center;
+}
+.btn-text-intro {
+  font-weight: 800;
+  font-size: 14px;
+  background-color: #34495e;
+  color: #ffffff;
+  padding: 10px 20px;
+  border-radius: 15px;
+  display: inline-block;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="main-header">
+    <img src="@/images/iron-logo.png" alt="ironlogo" class="iron-logo" />
+    <img src="@/images/menu-header.png" alt="menuheader" class="menu-header" />
+  </header>
+</template>
+
+<script>
+</script>
+
+<style>
+.main-header {
+  width: 540px;
+  height: 38px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.iron-logo {
+  width: 35px;
+  height: 38px;
+}
+.menu-header {
+  width: 22px;
+  height: 20px;
+}
+</style>
